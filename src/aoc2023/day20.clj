30ca43eedd75fcfe5ad12c66fdd63c0d6e47fccd
David Wagner <wagdav@gmail.com>
2023-11-29 22:01:38 +0100

    Generate empty files for all days

---
+++ src/aoc2023/day20.clj
@@ -0,0 +1,10 @@
+(ns aoc2023.day20
+  (:require [clojure.string :as str]))
+
+(defn parse-input [input]
+  (->> (re-seq #"-?\d+" input)
+       (mapv parse-long)))
+
+(defn solve-part1 [input])
+
+(defn solve-part2 [input])
---
+++ test/aoc2023/day11_test.clj
@@ -0,0 +1,16 @@
+(ns aoc2023.day11-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [aoc2023.day11 :refer [parse-input solve-part1 solve-part2]]
+            [clojure.java.io :as io]))
+
+(def example-input "")
+
+(deftest works
+  (testing "with example input"
+    (is (nil? (solve-part1 (parse-input example-input))))
+    (is (nil? (solve-part2 (parse-input example-input)))))
+
+  (testing "with real input"
+    (let [input (parse-input (slurp (io/resource "day11.txt")))]
+      (is (nil? (solve-part1 input)))
+      (is (nil? (solve-part2 input))))))
