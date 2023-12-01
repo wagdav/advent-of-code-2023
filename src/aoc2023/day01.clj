e71829cb535255760735a965b838a09d708f30df
David Wagner <wagdav@gmail.com>
2023-12-01 23:11:29 +0100

    day01: add smart solution

--- src/aoc2023/day01.clj
+++ src/aoc2023/day01.clj
@@ -23,10 +23,26 @@
                first)]
    (parse-long (str f l))))
 
+(defn line->calibration2* [line]
+  (->> (-> line
+           (str/replace "one"   "o1e")
+           (str/replace "two"   "t2o")
+           (str/replace "three" "t3e")
+           (str/replace "four"  "f4r")
+           (str/replace "five"  "f5e")
+           (str/replace "six"   "s6x")
+           (str/replace "seven" "s7n")
+           (str/replace "eight" "e8t")
+           (str/replace "nine"  "n9e"))
+       (re-seq #"\d")
+       ((juxt first last))
+       (apply str)
+       (parse-long)))
+
 (defn solve-part1 [input]
   (->> (map line->calibration1 input)
        (apply +)))
 
 (defn solve-part2 [input]
-  (->> (map line->calibration2 input)
+  (->> (map line->calibration2* input)
        (apply +)))
