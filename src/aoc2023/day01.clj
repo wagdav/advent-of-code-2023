315283a982a714cd07e5e56e441f746bcdfe73e4
David Wagner <wagdav@gmail.com>
2023-12-01 11:37:40 +0100

    Solve day01

--- src/aoc2023/day01.clj
+++ src/aoc2023/day01.clj
@@ -2,9 +2,31 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (str/split-lines input))
 
-(defn solve-part1 [input])
+(defn line->calibration1 [line]
+  (->> (re-seq #"\d" line)
+       ((juxt first last))
+       (apply str)
+       (parse-long)))
 
-(defn solve-part2 [input])
+(def digits {"one" "1", "two" "2", "three" "3", "four" "4", "five" "5", "six" "6", "seven" "7", "eight" "8", "nine" "9"})
+
+(defn line->calibration2 [line]
+  (let [f (->> (str/replace line (re-pattern (str/join "|" (keys digits))) digits)
+               (re-seq #"\d")
+               first)
+        l (->> (str/replace (str/reverse line)
+                            (re-pattern (->> (keys digits) (map str/reverse) (str/join "|")))
+                            (update-keys digits str/reverse))
+               (re-seq #"\d")
+               first)]
+   (parse-long (str f l))))
+
+(defn solve-part1 [input]
+  (->> (map line->calibration1 input)
+       (apply +)))
+
+(defn solve-part2 [input]
+  (->> (map line->calibration2 input)
+       (apply +)))
--- test/aoc2023/day01_test.clj
+++ test/aoc2023/day01_test.clj
@@ -3,14 +3,25 @@
             [aoc2023.day01 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input1 "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet")
+
+(def example-input2 "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 142 (solve-part1 (parse-input example-input1))))
+    (is (= 281 (solve-part2 (parse-input example-input2)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day01.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 55477 (solve-part1 input)))
+      (is (= 54431 (solve-part2 input))))))
