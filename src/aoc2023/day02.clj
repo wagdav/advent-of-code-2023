df8db403861c083d317d72b91ff2cd8e3e9235b0
David Wagner <wagdav@gmail.com>
2023-12-02 06:54:58 +0100

    Solve day02

--- src/aoc2023/day02.clj
+++ src/aoc2023/day02.clj
@@ -1,10 +1,52 @@
 (ns aoc2023.day02
   (:require [clojure.string :as str]))
 
+(defn parse-bag [bags]
+  (->> (str/split bags #",")
+       (map str/trim)
+       (map #(str/split % #" "))
+       (map (fn [[n c]] [(keyword c) (parse-long n)]))
+       (into {})))
+
+(defn parse-line [line]
+  (let [parts (str/split line #":|;")
+        bags (map parse-bag (drop 1 parts))]
+    bags))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (map parse-line (str/split-lines input)))
 
-(defn solve-part1 [input])
+(def bag {:red 12 :green 13 :blue 14})
 
-(defn solve-part2 [input])
+(defn possible? [game]
+  (every?
+    (fn [{:keys [red green blue] :or {red 0 green 0 blue 0}}]
+      (and (<= red (bag :red))
+           (<= green (bag :green))
+           (<= blue (bag :blue))))
+    game))
+
+(defn required-cubes [game]
+  (reduce
+    (fn [res {:keys [red green blue] :or {red 0 green 0 blue 0}}]
+      (-> res
+        (update :red max red)
+        (update :green max green)
+        (update :blue max blue)))
+    {:red 0 :green 0 :blue 0}
+    game))
+
+(defn power [game]
+  (apply * (vals (required-cubes game))))
+
+(defn solve-part1 [input]
+  (->> input
+       (map-indexed vector)
+       (filter (fn [[_i game]] (possible? game)))
+       (map (fn [[i _game]] (inc i)))
+       (apply +)))
+
+(defn solve-part2 [input]
+  (->> input
+       (map power)
+       (apply +)))
--- test/aoc2023/day02_test.clj
+++ test/aoc2023/day02_test.clj
@@ -3,14 +3,18 @@
             [aoc2023.day02 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 8 (solve-part1 (parse-input example-input))))
+    (is (= 2286 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day02.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 2101 (solve-part1 input)))
+      (is (= 58269 (solve-part2 input))))))
