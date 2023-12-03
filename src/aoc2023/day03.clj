ed33bab7c2d99ffb928b6bff8c2c001563f01b99
David Wagner <wagdav@gmail.com>
2023-12-03 09:22:22 +0100

    Solve day03.

--- src/aoc2023/day03.clj
+++ src/aoc2023/day03.clj
@@ -1,10 +1,60 @@
 (ns aoc2023.day03
   (:require [clojure.string :as str]))
 
+(defn parse-symbols [lines]
+  (into {}
+    (for [[row line] (map-indexed vector lines)
+          [col c] (map-indexed vector line)
+          :when (and (not (Character/isDigit c))
+                     (not= c \.))]
+      [[row col] c])))
+
+(defn parse-number-row [row line]
+  (second
+    (reduce
+      (fn [[col numbers] s]
+        [(+ col (count s))
+         (if (every? #(Character/isDigit %) s)
+           (conj numbers [[row col] (str/join s)])
+           numbers)])
+      [0 []]
+      (partition-by #(Character/isDigit %) line))))
+
+(defn parse-numbers [lines]
+  (into {}
+    (for [[row line] (map-indexed vector lines)
+          n (parse-number-row row line)]
+      n)))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [lines (str/split-lines input)]
+    {:symbols (parse-symbols lines)
+     :numbers (parse-numbers lines)}))
 
-(defn solve-part1 [input])
+(defn around [[[row col] digits]]
+  (set
+    (concat
+      ; line above and below
+      (for [c (range (dec col) (inc (+ col (count digits))))
+            r [(dec row) (inc row)]]
+        [r c])
+      ; same line
+      [[row (dec col)] [row (+ col (count digits))]])))
 
-(defn solve-part2 [input])
+(defn solve-part1 [{:keys [symbols numbers]}]
+  (let [symbol-coords (set (keys symbols))]
+    (->> numbers
+         (keep #(when (some symbol-coords (around %))
+                  (parse-long (second %))))
+         (apply +))))
+
+(defn solve-part2 [{:keys [symbols numbers]}]
+  (let [gear-coords (keep (fn [[c s]] (when (= s \*) c)) symbols)
+        neigbours-map (into {} (for [n numbers] [n (around n)]))]
+    (apply +
+      (for [g gear-coords
+            :let [adjacent (for [n numbers
+                                 :when (contains? (neigbours-map n) g)]
+                             (parse-long (second n)))]
+            :when (= (count adjacent) 2)]
+        (apply * adjacent)))))
--- test/aoc2023/day03_test.clj
+++ test/aoc2023/day03_test.clj
@@ -3,14 +3,24 @@
             [aoc2023.day03 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 4361 (solve-part1 (parse-input example-input))))
+    (is (= 467835 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day03.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 525181 (solve-part1 input)))
+      (is (= 84289137 (solve-part2 input))))))
