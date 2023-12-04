84a7b44fc00e8dfff90ba5d248d765ca25182a9a
David Wagner <wagdav@gmail.com>
2023-12-04 06:41:23 +0100

    Solve day04

--- src/aoc2023/day04.clj
+++ src/aoc2023/day04.clj
@@ -1,10 +1,54 @@
 (ns aoc2023.day04
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :refer [pow]]))
 
-(defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
+(def example-input "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+")
+
+(defn parse-numbers [line]
+  (->> (re-seq #"-?\d+" line)
        (mapv parse-long)))
 
-(defn solve-part1 [input])
+(defn parse-line [line]
+  (let [[c w d] (str/split line #":|\|")]
+    [(dec (first (parse-numbers c))) (set (parse-numbers w)) (parse-numbers d)]))
 
-(defn solve-part2 [input])
+(defn parse-input [input]
+  (->> (str/split-lines input)
+       (mapv parse-line)))
+
+(defn matches [[_ winning drawn]]
+  (count (filter winning drawn)))
+
+(defn score [card]
+  (let [m (matches card)]
+    (if (zero? m)
+      0
+      (long (pow 2 (dec m))))))
+
+(defn win-copies [[id _ _ :as card]]
+  (range (inc id) (+ 1 id (matches card))))
+
+(defn solve-part1 [input]
+  (apply + (map score input)))
+
+(defn process [cards total won]
+  (if (empty? won)
+    total
+    (recur
+      cards
+      (merge-with + total won)
+      (apply merge-with +
+        (for [[id qty] won]
+          (update-vals
+            (frequencies (win-copies (cards id)))
+            #(* qty %)))))))
+
+(defn solve-part2 [input]
+  (apply +
+   (vals (process input {} (zipmap (range (count input)) (repeat 1))))))
--- test/aoc2023/day04_test.clj
+++ test/aoc2023/day04_test.clj
@@ -3,14 +3,20 @@
             [aoc2023.day04 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
+    (is (= 13 (solve-part1 (parse-input example-input))))
     (is (nil? (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day04.txt")))]
-      (is (nil? (solve-part1 input)))
+      (is (= 25651 (solve-part1 input)))
       (is (nil? (solve-part2 input))))))
