cb8b46909a01aa0a690722b9e0c5d803efa5c4ce
David Wagner <wagdav@gmail.com>
2023-12-06 08:19:36 +0100

    Solve day06

--- src/aoc2023/day06.clj
+++ src/aoc2023/day06.clj
@@ -1,10 +1,37 @@
 (ns aoc2023.day06
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :as math]))
+
+(defn transpose [v]
+  (apply mapv vector v))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (str/split-lines input))
 
-(defn solve-part1 [input])
+(defn wins-naive [[race-time record]]
+   (count (->> (range race-time)
+               (map #(* % (- race-time %)))
+               (filter #(> % record)))))
 
-(defn solve-part2 [input])
+(defn wins-fast
+  [[race-time record]]
+  ; The distance convered is d = wait * (race-time - wait).  This is quadradic
+  ; in wait.  Solve for wait and compute the difference between the roots.
+  (let [d (math/sqrt (- (math/pow race-time 2) (* 4 record)))
+        w1 (/ (+ race-time d) 2)
+        w2 (/ (- race-time d) 2)]
+    (long (dec (- (math/ceil w1) (math/floor w2))))))
+
+(defn solve-part1 [input]
+  (let [races (->> input
+                   (map #(re-seq #"-?\d+" %))
+                   (map #(map parse-long %))
+                   transpose)]
+    (apply * (map wins-fast races))))
+
+(defn solve-part2 [input]
+  (let [big-race (->> input
+                      (map #(re-seq #"-?\d+" %))
+                      (map str/join)
+                      (map parse-long))]
+    (wins-fast big-race)))
--- test/aoc2023/day06_test.clj
+++ test/aoc2023/day06_test.clj
@@ -3,14 +3,16 @@
             [aoc2023.day06 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "Time:      7  15   30
+Distance:  9  40  200
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 288 (solve-part1 (parse-input example-input))))
+    (is (= 71503 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day06.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 625968 (solve-part1 input)))
+      (is (= 43663323 (solve-part2 input))))))
