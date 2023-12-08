755ffb749a2f0f59fdd39ae9da318efc9e4c2894
David Wagner <wagdav@gmail.com>
2023-12-08 07:42:50 +0100

    Solve day08

--- src/aoc2023/day08.clj
+++ src/aoc2023/day08.clj
@@ -2,9 +2,38 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[turns & nodes] (re-seq #"\w+" input)]
+   {:turns turns
+    :nodes (->> nodes
+                (partition 3)
+                (map (fn [[f l r]] [f [l r]]))
+                (into {}))
+    :start (filter #(str/ends-with? % "A") nodes)}))
 
-(defn solve-part1 [input])
+(defn solve-part1 [{:keys [turns nodes]}]
+  (reduce
+    (fn [[steps pos] turn]
+      (if (= pos "ZZZ")
+        (reduced steps)
+        (let [[l r] (nodes pos)]
+          [(inc steps) ({\L l \R r} turn)])))
+    [0 "AAA"]
+    (cycle turns)))
 
-(defn solve-part2 [input])
+(defn lcm
+  "Least common multiple of a and b"
+  [a b]
+  (.divide
+    (.multiply (biginteger a) (biginteger b))
+    (.gcd (biginteger a) (biginteger b))))
+
+(defn solve-part2 [{:keys [turns nodes start]}]
+  (loop [steps 0, ps start, turns (cycle turns), periods {}]
+   (if (= (count periods) (count start))
+     (reduce lcm (vals periods))
+     (let [to (for [[l r] (map nodes ps)] ({\L l \R r} (first turns)))
+           return (into {}
+                        (for [[i p] (map-indexed vector ps)
+                              :when (str/ends-with? p "Z")]
+                          [i steps]))]
+       (recur (inc steps) to (rest turns) (merge periods return))))))
--- test/aoc2023/day08_test.clj
+++ test/aoc2023/day08_test.clj
@@ -3,14 +3,29 @@
             [aoc2023.day08 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input1 "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)")
+
+(def example-input2 "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 6 (solve-part1 (parse-input example-input1))))
+    (is (= 6 (solve-part2 (parse-input example-input2)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day08.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 20777 (solve-part1 input)))
+      (is (= 13289612809129 (solve-part2 input))))))
