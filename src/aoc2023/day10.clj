cfeafbd74864c98965e3c42e0eab12dc225822ab
David Wagner <wagdav@gmail.com>
2023-12-10 08:49:49 +0100

    Solve day 10

--- src/aoc2023/day10.clj
+++ src/aoc2023/day10.clj
@@ -1,10 +1,101 @@
 (ns aoc2023.day10
   (:require [clojure.string :as str]))
 
+(def north [-1  0])
+(def south [ 1  0])
+(def east  [ 0  1])
+(def west  [ 0 -1])
+
+(def tiles {\| #{north south}
+            \- #{east west}
+            \L #{north east}
+            \J #{north west}
+            \7 #{south west}
+            \F #{south east}})
+
+(defn add [p1 p2]
+  (mapv + p1 p2))
+
+(defn connected [pipes pos]
+  (map #(add pos %) (tiles (get pipes pos))))
+
+(defn fill-start-tile [pipes start]
+  (some
+    (fn [tile]
+      (let [pipes* (assoc pipes start tile)
+            [e1 e2] (connected pipes* start)]
+        (when (and (contains? (set (connected pipes* e1)) start)
+                   (contains? (set (connected pipes* e2)) start))
+          pipes*)))
+    (keys tiles)))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [pipes (into {}
+                (for [[r line] (map-indexed vector (str/split-lines input))
+                      [c p] (map-indexed vector line)]
+                  [[r c] p]))
+        start (->> pipes (filter #(= (second %) \S)) ffirst)]
+    {:pipes (fill-start-tile pipes start)
+     :start start}))
 
-(defn solve-part1 [input])
+(defn solve-part1 [{:keys [pipes start]}]
+  (loop [c 1, frontier (set (connected pipes start)), visited #{start}]
+    (let [new-frontier (->> frontier
+                            (mapcat #(connected pipes %))
+                            (remove visited))]
+      (if (empty? new-frontier)
+        c
+        (recur (inc c) new-frontier (into visited frontier))))))
 
-(defn solve-part2 [input])
+(def left  {north west, east north, south east, west south})
+(def right {north east, east south, south west, west north})
+
+(defn follow
+  "From a given position follow the pipes in the direction. Returns the new
+  position and new direction."
+  [pipes pos dir]
+  (let [new-pos (add pos dir)
+        [d1 d2] (vec (tiles (pipes new-pos)))]
+    [new-pos (if (= (add new-pos d1) pos) d2 d1)]))
+
+(defn trace-sides [pos dir]
+  (let [l (add pos (left dir))
+        r (add pos (right dir))
+        ahead (add pos dir)
+        lc (add ahead (left dir))
+        rc (add ahead (right dir))]
+    [#{l lc} #{r rc}]))
+
+(defn flood [pipes path ps]
+  (loop [points (set ps), frontier (set ps)]
+    (let [new-frontier (set (for [dir [[0 1] [0 -1] [-1 0] [1 0]]
+                                  p frontier
+                                  :let [f (add p dir)]
+                                  :when (and (pipes f)
+                                             (not (path f))
+                                             (not (points f)))]
+                              f))]
+      (if (empty? new-frontier)
+        points
+        (recur (into points new-frontier) new-frontier)))))
+
+(defn solve-part2 [{:keys [pipes start]}]
+  (loop [pos start
+         dir (first (tiles (pipes start)))
+         path #{start}
+         lefts #{}
+         rights #{}]
+    (let [[next-pos, next-dir] (follow pipes pos dir)]
+      (if (= next-pos start)
+        (apply min ; Assumption: less tiles inside the loop than outside
+          (for [ps [lefts rights]]
+            (->> ps
+                 (remove path)
+                 (flood pipes path)
+                 count)))
+        (let [[next-lefts next-rights] (trace-sides pos dir)]
+          (recur next-pos
+                 next-dir
+                 (conj path next-pos)
+                 (into lefts next-lefts)
+                 (into rights next-rights)))))))
--- test/aoc2023/day10_test.clj
+++ test/aoc2023/day10_test.clj
@@ -3,14 +3,52 @@
             [aoc2023.day10 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+")
+
+(def example-four-enclosed "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........")
+
+(def example-four-enclosed-squeeze "..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........")
+
+(def example-large "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 8 (solve-part1 (parse-input example-input))))
+    (is (= 4 (solve-part2 (parse-input example-four-enclosed))))
+    (is (= 4 (solve-part2 (parse-input example-four-enclosed-squeeze))))
+    (is (= 10 (solve-part2 (parse-input example-large)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day10.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 6979 (solve-part1 input)))
+      (is (= 443 (solve-part2 input))))))
