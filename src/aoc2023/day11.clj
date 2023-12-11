7ab9497f61d28a9a3849758aceaad0564f652ce9
David Wagner <wagdav@gmail.com>
2023-12-11 08:01:06 +0100

    Solve day11

--- src/aoc2023/day11.clj
+++ src/aoc2023/day11.clj
@@ -1,10 +1,64 @@
 (ns aoc2023.day11
   (:require [clojure.string :as str]))
 
+(defn empty-coords [rows]
+  (for [[i row] (map-indexed vector rows)
+        :when (= (set row) #{\.})]
+    i))
+
+(defn transpose [v]
+  (apply mapv vector v))
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [lines (str/split-lines input)]
+    {:galaxies
+     (for [[i row] (map-indexed vector lines)
+           [j c]  (map-indexed vector row)
+           :when (= c \#)]
+       [i j])
+     :empty-rows (empty-coords lines)
+     :empty-cols (empty-coords (transpose lines))}))
 
-(defn solve-part1 [input])
+(defn expand-rows [galaxies empty-rows factor]
+  (loop [gs galaxies, rs empty-rows]
+    (if (seq rs)
+      (recur
+        (map
+          (fn [[row col]] (if (< row (first rs))
+                            [row col]
+                            [(+ row (dec factor)) col]))
+          gs)
+        (map #(+ % (dec factor)) (rest rs)))
+      gs)))
 
-(defn solve-part2 [input])
+(defn expand-cols [galaxies empty-cols factor]
+  (loop [gs galaxies, cs empty-cols]
+    (if (seq cs)
+      (recur
+        (map
+          (fn [[row col]] (if (< col (first cs))
+                            [row col]
+                            [row (+ col (dec factor))]))
+          gs)
+        (map #(+ % (dec factor)) (rest cs)))
+      gs)))
+
+(defn expand [{:keys [galaxies empty-rows empty-cols]} factor]
+  (-> galaxies
+      (expand-rows empty-rows factor)
+      (expand-cols empty-cols factor)))
+
+(defn manhattan [p1 p2]
+  (->> (map - p1 p2)
+       (map abs)
+       (apply +)))
+
+(defn distances [input factor]
+  (let [gs (expand input factor)]
+    (/ (apply + (for [g1 gs, g2 gs] (manhattan g1 g2))) 2)))
+
+(defn solve-part1 [input]
+  (distances input 2))
+
+(defn solve-part2 [input]
+  (distances input 1000000))
--- test/aoc2023/day11_test.clj
+++ test/aoc2023/day11_test.clj
@@ -1,16 +1,27 @@
 (ns aoc2023.day11-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc2023.day11 :refer [parse-input solve-part1 solve-part2]]
+            [aoc2023.day11 :refer [parse-input distances solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 374 (solve-part1 (parse-input example-input))))
+    (is (= 1030 (distances (parse-input example-input) 10)))
+    (is (= 8410 (distances (parse-input example-input) 100))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day11.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 9233514 (solve-part1 input)))
+      (is (= 363293506944 (solve-part2 input))))))
