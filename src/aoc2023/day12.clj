b4be87b2500571aa9647fdccc5945fc8c4785d55
David Wagner <wagdav@gmail.com>
2023-12-12 09:19:31 +0100

    Solve day 12

--- src/aoc2023/day12.clj
+++ src/aoc2023/day12.clj
@@ -2,9 +2,67 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (->> (str/split-lines input)
+       (map
+         (fn [line]
+           (let [[springs code] (str/split line #" ")]
+             [springs (map parse-long (re-seq #"\d+" code))])))))
 
-(defn solve-part1 [input])
+(defn match? [pattern v]
+  (assert (<= (count v) (count pattern)))
+  (loop [vs, v, ps pattern]
+   (if (seq vs)
+     (if (or (= \? (first ps))
+             (= (first ps) (first vs)))
+       (recur (rest vs) (rest ps))
+       false)
+     true)))
 
-(defn solve-part2 [input])
+(defn arrangements
+ ([[pattern groups]]
+  (arrangements [pattern groups] (- (count pattern) (apply + groups)) true))
+ ([[pattern groups] operational first?]
+  (if (seq groups)
+    (for [start (->> (range (if first? 0 1) (inc operational))
+                     (map #(str/join (repeat % ".")))
+                     (map #(apply str %)))
+          body (arrangements [(subs pattern (+ (count start) (first groups))) (rest groups)]
+                             (- operational (count start))
+                             false)
+          :let [middle (str/join (repeat (first groups) \#))
+                res (str/join [start middle body])]
+          :when (match? pattern (str/join [start middle]))]
+      res)
+    (->> [(str/join (repeat operational \.))]
+         (filter #(match? pattern %))))))
+
+(def count-arrangements*
+  (memoize
+    (fn [[pattern groups] operational first?]
+      (if (seq groups)
+        (let [starts (->> (range (if first? 0 1) (inc operational))
+                          (map #(str/join (repeat % ".")))
+                          (map #(str/join [% (str/join (repeat (first groups) \#))]))
+                          (filter #(match? pattern %)))]
+          (apply +
+            (for [s starts]
+              (count-arrangements* [(subs pattern (count s)) (rest groups)]
+                                   (- operational (- (count s) (first groups)))
+                                   false))))
+        (->> [(str/join (repeat operational \.))]
+             (filter #(match? pattern %))
+             count)))))
+
+(defn count-arrangements [[pattern groups]]
+  (count-arrangements* [pattern groups] (- (count pattern) (apply + groups)) true))
+
+(defn solve-part1 [input]
+  (apply + (map count (map arrangements input))))
+
+(defn unfold [[pattern sig]]
+  [(str/join "?" (repeat 5 pattern))
+   (apply concat (repeat 5 sig))])
+
+(defn solve-part2 [input]
+  (let [unfolded-input (map unfold input)]
+    (apply + (map count-arrangements unfolded-input))))
--- test/aoc2023/day12_test.clj
+++ test/aoc2023/day12_test.clj
@@ -3,14 +3,20 @@
             [aoc2023.day12 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 21 (solve-part1 (parse-input example-input))))
+    (is (= 525152 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day12.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 6949 (solve-part1 input)))
+      (is (= 51456609952403 (solve-part2 input))))))
