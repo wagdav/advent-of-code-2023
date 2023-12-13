eb7a1147051ba4ae2396a3df4c5937cbc591f9d0
David Wagner <wagdav@gmail.com>
2023-12-13 10:36:04 +0100

    Day 13

--- src/aoc2023/day13.clj
+++ src/aoc2023/day13.clj
@@ -2,9 +2,61 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [grids (str/split input #"\n\n")]
+    (map str/split-lines grids)))
 
-(defn solve-part1 [input])
+(defn transpose [v]
+  (apply mapv vector v))
 
-(defn solve-part2 [input])
+(defn repeated [rows]
+  (filter (fn [i] (= (get rows i) (get rows (inc i))))
+          (range (dec (count rows)))))
+
+(defn mirror? [rows c]
+  (assert (= (get rows c) (get rows (inc c))))
+  (loop [delta 0]
+    (if (or (neg? (- c delta))
+            (<= (count rows) (+ c (inc delta))))
+      true
+      (let [r1 (get rows (- c delta))
+            r2 (get rows (+ c (inc delta)))]
+        (if (= r1 r2)
+          (recur (inc delta))
+          false)))))
+
+(defn row-mirrors [rows]
+  (for [cc (repeated rows) :when (mirror? rows cc)] cc))
+
+(defn mirrors [grid]
+  (set
+    (concat
+      (for [m (row-mirrors grid)] [:horizontal (inc m)])
+      (for [m (row-mirrors (transpose grid))] [:vertical (inc m)]))))
+
+(defn score [[hv pos]]
+  (case hv
+    :horizontal (* 100 pos)
+    :vertical pos))
+
+(defn solve-part1 [input]
+  (apply +
+    (for [g input]
+      (score (first (mirrors g))))))
+
+(defn unsmudge [grid]
+  (let [ms (mirrors grid)]
+    (first
+      (for [i (range (count grid))
+            j (range (count (first grid)))
+            :let [u (mapv str/join
+                      (-> (mapv vec grid)
+                          (update-in [i j] {\# \., \. \#})))
+                  ums (set (remove ms (mirrors u)))]
+            :when (seq ums)]
+        (first ums)))))
+
+(defn solve-part2 [input]
+  (->> input
+       (map unsmudge)
+       (map score)
+       (apply +)))
--- test/aoc2023/day13_test.clj
+++ test/aoc2023/day13_test.clj
@@ -3,14 +3,28 @@
             [aoc2023.day13 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 405 (solve-part1 (parse-input example-input))))
+    (is (= 400 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day13.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 29846 (solve-part1 input)))
+      (is (= 25401 (solve-part2 input))))))
