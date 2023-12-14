4962b6a3bfefe3e35d71059e9a326b359da4ecac
David Wagner <wagdav@gmail.com>
2023-12-14 07:32:45 +0100

    Solve day 14

--- src/aoc2023/day14.clj
+++ src/aoc2023/day14.clj
@@ -2,9 +2,62 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (mapv vec (str/split-lines input)))
 
-(defn solve-part1 [input])
+(defn rocks [dish]
+  (for [i (range (count dish))
+        j (range (count (first dish)))
+        :when (= \O (get-in dish [i j]))]
+    [i j]))
 
-(defn solve-part2 [input])
+(def directions {:north [-1 0] :south [1 0] :west [0 -1] :east [0 1]})
+
+(defn roll
+ ([dish dir]
+  (reduce
+    (fn [g rock] (roll g rock dir))
+    dish
+    (rocks dish)))
+ ([dish pos dir]
+  (let [target (map + pos (directions dir))]
+    (assert (= \O (get-in dish pos)))
+    (if-let [c (get-in dish target)]
+      (if (= \. c)
+        (-> dish
+            (assoc-in pos \.)
+            (assoc-in target \O))
+        dish)
+      dish))))
+
+(defn tilt [dish direction]
+  (loop [g dish]
+    (let [new-g (roll g direction)]
+      (if (= new-g g)
+        g
+        (recur new-g)))))
+
+(defn total-load [dish]
+  (apply +
+    (for [[r _] (rocks dish)]
+      (- (count dish) r))))
+
+(defn solve-part1 [input]
+  (total-load (tilt input :north)))
+
+(defn spin-cycle [dish cycles]
+  (loop [c 1, g dish, sigs {}]
+    (if (= c cycles)
+      g
+      (recur
+        (if (sigs g) ; Found a repeated state
+          (let [period (- c (sigs g))
+                remaining (- cycles c)]
+            (+ c (* period (quot remaining period)))) ; Fast-forward
+          (inc c))
+        (-> g (tilt :north) (tilt :west) (tilt :south) (tilt :east))
+        (if (sigs g) {} (assoc sigs g c))))))
+
+(def billion 1000000000)
+
+(defn solve-part2 [input]
+  (total-load (spin-cycle input billion)))
--- test/aoc2023/day14_test.clj
+++ test/aoc2023/day14_test.clj
@@ -3,14 +3,24 @@
             [aoc2023.day14 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 136 (solve-part1 (parse-input example-input))))
+    (is (= 64 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day14.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (time (= 109638 (solve-part1 input))))
+      #_(is (time (= 102657 (solve-part2 input))))))) ; slow, runs in ~36s
