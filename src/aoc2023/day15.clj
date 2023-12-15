edc65bb34a4eaec1707355eed435c39637af5458
David Wagner <wagdav@gmail.com>
2023-12-15 07:24:59 +0100

    Solve day 15

--- src/aoc2023/day15.clj
+++ src/aoc2023/day15.clj
@@ -2,9 +2,39 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (str/split (str/trim input) #","))
 
-(defn solve-part1 [input])
+(defn elf-hash [s]
+  (reduce
+    (fn [res c]
+      (-> (int c)
+          (+ res)
+          (* 17)
+          (rem 256)))
+    0
+    s))
 
-(defn solve-part2 [input])
+(defn parse-instruction [s]
+  (let [[label op f] (re-seq #"\w+|[=-]|\d+" s)]
+    (cond-> [op label]
+            f (conj (parse-long f)))))
+
+(defn elf-hash-map [boxes ins]
+  (let [[op label f] (parse-instruction ins)]
+    (update boxes (elf-hash label)
+      (fn [box]
+        (case op
+          "=" (assoc box label f)
+          "-" (dissoc box label))))))
+
+(defn focusing-power [boxes]
+  (apply +
+    (for [[box-id lenses] boxes
+          [i [_ f]] (map-indexed vector lenses)]
+      (* (inc box-id) (inc i) f))))
+
+(defn solve-part1 [input]
+  (apply + (map elf-hash input)))
+
+(defn solve-part2 [input]
+  (focusing-power (reduce elf-hash-map {} input)))
--- test/aoc2023/day15_test.clj
+++ test/aoc2023/day15_test.clj
@@ -3,14 +3,14 @@
             [aoc2023.day15 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 1320 (solve-part1 (parse-input example-input))))
+    (is (= 145 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day15.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 521341 (solve-part1 input)))
+      (is (= 252782 (solve-part2 input))))))
