ef182c84deb0e5eb2caada1ed7d007141afda778
David Wagner <wagdav@gmail.com>
2023-12-18 07:31:30 +0100

    Day 18

--- src/aoc2023/day18.clj
+++ src/aoc2023/day18.clj
@@ -1,10 +1,56 @@
 (ns aoc2023.day18
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :as math]
+            [clojure.java.io :as io]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (->> (str/split-lines input)
+       (map (fn [line]
+              (let [[d v c] (str/split line #" ")]
+                [d (parse-long v) c])))))
 
-(defn solve-part1 [input])
+(def directions {"L" [0 -1] "R" [0 1] "U" [-1 0] "D" [1 0]})
 
-(defn solve-part2 [input])
+(defn move [pos d q]
+  (let [delta (map #(* q %) (directions d))]
+    (mapv + pos delta)))
+
+(defn moves->points [moves]
+  (reduce
+    (fn [points [direction amount]]
+      (conj points (move (last points) direction amount)))
+    [[0 0]]
+    moves))
+
+; Pick's theorem:  A = i + b/2 - 1
+;     A = polygon's area
+;     i = number of interior points
+;     b = number of boundary points
+;
+; The lagoon's size is (i + b), which comes from rearranging Pick's theorem:
+;
+;     i + b = A + b/2 + 1
+;
+; where A is computed using the Shoelace formula and b is the length of the
+; perimeter.
+(defn lagoon [points perimeter]
+  (let [shoelace (apply + (for [[[r1 c1] [r2 c2]] (partition 2 1 points)]
+                            (/ (- (* r1 c2) (* r2 c1)) 2)))]
+     (inc (+ (abs shoelace) (/ perimeter 2)))))
+
+(defn solve-part1 [input]
+  (let [points (moves->points input)
+        perimeter (apply + (map second input))]
+    (lagoon points perimeter)))
+
+(defn color->instruction [c]
+  (let [amt (subs c 2 7)
+        d (subs c 7 8)
+        table {0 "R" 1 "D" 2 "L" 3 "U"}]
+    [(table (parse-long d)) (Integer/parseInt amt 16)]))
+
+(defn solve-part2 [input]
+  (let [moves (for [[_ _ color] input] (color->instruction color))
+        perimeter (apply + (map second moves))
+        points (moves->points moves)]
+    (lagoon points perimeter)))
--- test/aoc2023/day18_test.clj
+++ test/aoc2023/day18_test.clj
@@ -3,14 +3,28 @@
             [aoc2023.day18 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 62 (solve-part1 (parse-input example-input))))
+    (is (= 952408144115 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day18.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 40761 (solve-part1 input)))
+      (is (= 106920098354636 (solve-part2 input))))))
