d3b1e6ee959089fbaa4bf511d2e30c0ff82585d8
David Wagner <wagdav@gmail.com>
2023-12-19 06:25:46 +0100

    Day 19

--- src/aoc2023/day19.clj
+++ src/aoc2023/day19.clj
@@ -1,10 +1,104 @@
 (ns aoc2023.day19
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn parse-rule [line]
+  (let [[r target] (str/split line #":")
+        [p c limit] (re-seq #"[asxm]|\d+|[<>]" line)]
+    {:category p
+     :condition c
+     :limit (parse-long limit)
+     :target target}))
+
+(defn parse-workflow [line]
+  (let [[n ws] (str/split line #"[{}]")
+        rules (str/split ws #",")]
+    [n {:rules (map parse-rule (butlast rules))
+        :default (last rules)}]))
+
+(defn parse-part [line]
+  (let [ps (re-seq #"[asxm]|\d+" line)]
+    (into {}
+      (for [[n v] (partition 2 ps)]
+        [n (parse-long v)]))))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[b1 b2] (str/split input #"\n\n")
+        ws (map parse-workflow (str/split-lines b1))
+        ps (map parse-part (str/split-lines b2))]
+    {:workflows (into {} ws)
+     :parts ps}))
 
-(defn solve-part1 [input])
+(defn run-workflow [{:keys [rules default]} part]
+  (loop [rs rules]
+    (if (seq rs)
+      (let [{:keys [condition category limit target]} (first rs)
+            cond-fn ({">" > "<" <} condition)]
+        (if (cond-fn (get part category) limit)
+          target
+          (recur (rest rs))))
+      default)))
 
-(defn solve-part2 [input])
+(defn run [workflows part]
+  (loop [n "in", w (workflows "in")]
+    (let [result (run-workflow w part)]
+      (if (#{"R" "A"} result)
+        result
+        (recur result (workflows result))))))
+
+(defn solve-part1 [{:keys [parts workflows]}]
+  (->> parts
+       (filter #(= "A" (run workflows %)))
+       (map vals)
+       (map #(apply + %))
+       (apply +)))
+
+(defn run2 [workflows ranges rules]
+  (loop [accepted [], workqueue [[ranges rules]]]
+    (if (empty? workqueue)
+      accepted
+      (let [[part {:keys [rules default] :as workflow}] (first workqueue)
+            [dst to-target to-next]
+            (if (empty? rules)
+              [default part nil] ; no more rules, send part to default
+              (let [{:keys [condition category limit target]} (first rules)
+                    [lo hi] (part category)]
+                (case condition
+                  "<"  (cond (< lo hi limit)
+                             ; send whole to target, nothing to rest of the workflow
+                             [target part nil]
+                             (< lo limit hi)
+                             ; send matching to target, complement to rest of the workflow
+                             [target (assoc part category [lo (dec limit)])
+                                     (assoc part category [limit hi])]
+                             (< limit lo hi)
+                             ; send whole range to rest of the pipeline
+                             [target nil part])
+                  ">"  (cond (> hi lo limit)
+                             ; send whole to target, nothing to rest of the workflow
+                             [target part nil]
+                             (> hi limit lo)
+                             ; send matching to target, complement to rest of the workflow
+                             [target (assoc part category [(inc limit) hi]),
+                                     (assoc part category [lo limit])]
+                             (> limit hi lo)
+                             ; send whole range to rest of the pipeline
+                             [target nil part]))))]
+        (recur (if (= dst "A")
+                 (conj accepted to-target)
+                 accepted)
+               (cond-> (rest workqueue)
+                 to-next
+                 (conj [to-next (assoc workflow :rules (rest rules))])
+
+                 (and to-target (not (#{"A" "R"} dst)))
+                 (conj [to-target (workflows dst)])))))))
+
+(defn solve-part2 [{:keys [workflows]}]
+  (let [p {"x" [1 4000], "m" [1 4000], "a" [1 4000], "s" [1 4000]}
+        w (workflows "in")]
+    (apply +
+      (for [p (run2 workflows p w)]
+        (apply *
+          (for [[lo hi] (vals p)]
+            (- (inc hi) lo)))))))
--- test/aoc2023/day19_test.clj
+++ test/aoc2023/day19_test.clj
@@ -3,14 +3,31 @@
             [aoc2023.day19 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 19114 (solve-part1 (parse-input example-input))))
+    (is (= 167409079868000 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day19.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 509597 (solve-part1 input)))
+      (is (= 143219569011526 (solve-part2 input))))))
